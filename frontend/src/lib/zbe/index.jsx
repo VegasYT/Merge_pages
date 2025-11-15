9732865afbda9e153cc05aabf609a20ba2e4f4f2
simptom569 <pikalovegor0@gmail.com>
2025-11-15 10:03:29 +0300

    xdfsdf

--- frontend/src/lib/zbe/index.jsx
+++ frontend/src/lib/zbe/index.jsx
@@ -6,6 +6,7 @@ import App from './App.jsx';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <script src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"></script>
     <App />
   </React.StrictMode>
 );
